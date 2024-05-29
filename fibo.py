e9ad7b51b7265c82b13d5b968e7fc29c7ce37e9d
Tisola <10808786+TitouanTheophile@users.noreply.github.com>
2024-05-29 13:24:34 +0200

    init

---
+++ fibo.py
@@ -0,0 +1,20 @@
+# Fibonacci function using iteration
+def fibonacci_iterative(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        fib = [0, 1]
+        for i in range(2, n + 1):
+            fib.append(fib[i - 1] + fib[i - 2])
+        return fib[n]
+
+# Fibonacci function using recursion
+def fibonacci_recursive(n):
+    if n <= 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
