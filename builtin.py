24d0a4422eaa85aa28738725dfee9f63e644ba0a
Léo OUCH <youheng93160@hotmail.fr>
2024-05-29 16:03:26 +0200

    add methods skeletons + abs + bool

---
+++ builtin.py
@@ -0,0 +1,285 @@
+from builtins import abs
+
+def abs(*args):
+    # return abs(*args)
+    pass
+
+def aiter(*args):
+    # return aiter(*args)
+    pass
+
+def all(*args):
+    # return all(*args)
+    pass
+
+def anext(*args):
+    # return anext(*args)
+    pass
+
+def any(*args):
+    # return any(*args)
+    pass
+
+def ascii(*args):
+    # return ascii(*args)
+    pass
+
+def bin(*args):
+    # return bin(*args)
+    pass
+
+def bool(*args):
+    # return bool(*args)
+    pass
+
+def breakpoint(*args):
+    # return breakpoint(*args)
+    pass
+
+def bytearray(*args):
+    # return bytearray(*args)
+    pass
+
+def bytes(*args):
+    # return bytes(*args)
+    pass
+
+def callable(*args):
+    # return callable(*args)
+    pass
+
+def chr(*args):
+    # return chr(*args)
+    pass
+
+def classmethod(*args):
+    # return classmethod(*args)
+    pass
+
+def compile(*args):
+    # return compile(*args)
+    pass
+
+def complex(*args):
+    # return complex(*args)
+    pass
+
+def delattr(*args):
+    # return delattr(*args)
+    pass
+
+def dict(*args):
+    # return dict(*args)
+    pass
+
+def dir(*args):
+    # return dir(*args)
+    pass
+
+def divmod(*args):
+    # return divmod(*args)
+    pass
+
+def enumerate(*args):
+    # return enumerate(*args)
+    pass
+
+def eval(*args):
+    # return eval(*args)
+    pass
+
+def exec(*args):
+    # return exec(*args)
+    pass
+
+def filter(*args):
+    # return filter(*args)
+    pass
+
+def float(*args):
+    # return float(*args)
+    pass
+
+def format(*args):
+    # return format(*args)
+    pass
+
+def frozenset(*args):
+    # return frozenset(*args)
+    pass
+
+def getattr(*args):
+    # return getattr(*args)
+    pass
+
+def globals(*args):
+    # return globals(*args)
+    pass
+
+def hasattr(*args):
+    # return hasattr(*args)
+    pass
+
+def hash(*args):
+    # return hash(*args)
+    pass
+
+def help(*args):
+    # return help(*args)
+    pass
+
+def hex(*args):
+    # return hex(*args)
+    pass
+
+def id(*args):
+    # return id(*args)
+    pass
+
+def input(*args):
+    # return input(*args)
+    pass
+
+def int(*args):
+    # return int(*args)
+    pass
+
+def isinstance(*args):
+    # return isinstance(*args)
+    pass
+
+def issubclass(*args):
+    # return issubclass(*args)
+    pass
+
+def iter(*args):
+    # return iter(*args)
+    pass
+
+def len(*args):
+    # return len(*args)
+    pass
+
+def list(*args):
+    # return list(*args)
+    pass
+
+def locals(*args):
+    # return locals(*args)
+    pass
+
+def map(*args):
+    # return map(*args)
+    pass
+
+def max(*args):
+    # return max(*args)
+    pass
+
+def memoryview(*args):
+    # return memoryview(*args)
+    pass
+
+def min(*args):
+    # return min(*args)
+    pass
+
+def next(*args):
+    # return next(*args)
+    pass
+
+def object(*args):
+    # return object(*args)
+    pass
+
+def oct(*args):
+    # return oct(*args)
+    pass
+
+def open(*args):
+    # return open(*args)
+    pass
+
+def ord(*args):
+    # return ord(*args)
+    pass
+
+def pow(*args):
+    # return pow(*args)
+    pass
+
+def print(*args):
+    # return print(*args)
+    pass
+
+def property(*args):
+    # return property(*args)
+    pass
+
+def range(*args):
+    # return range(*args)
+    pass
+
+def repr(*args):
+    # return repr(*args)
+    pass
+
+def reversed(*args):
+    # return reversed(*args)
+    pass
+
+def round(*args):
+    # return round(*args)
+    pass
+
+def set(*args):
+    # return set(*args)
+    pass
+
+def setattr(*args):
+    # return setattr(*args)
+    pass
+
+def slice(*args):
+    # return slice(*args)
+    pass
+
+def sorted(*args):
+    # return sorted(*args)
+    pass
+
+def staticmethod(*args):
+    # return staticmethod(*args)
+    pass
+
+def str(*args):
+    # return str(*args)
+    pass
+
+def sum(*args):
+    # return sum(*args)
+    pass
+
+def super(*args):
+    # return super(*args)
+    pass
+
+def tuple(*args):
+    # return tuple(*args)
+    pass
+
+def type(*args):
+    # return type(*args)
+    pass
+
+def vars(*args):
+    # return vars(*args)
+    pass
+
+def zip(*args):
+    # return zip(*args)
+    pass
+
+# def import(*args):
+    # return import(*args)  pass
+    # pass
