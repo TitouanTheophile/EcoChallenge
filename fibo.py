20d886c96cb429aeb0ef14735574c223a5a7a736
m-bonati <69583646+m-bonati@users.noreply.github.com>
2024-05-29 14:01:59 +0200

    add main function

--- fibo.py
+++ fibo.py
@@ -1,4 +1,7 @@
 # Fibonacci function using iteration
+import click
+
+
 def fibonacci_iterative(n):
     if n <= 0:
         return 0
@@ -10,6 +13,7 @@ def fibonacci_iterative(n):
             fib.append(fib[i - 1] + fib[i - 2])
         return fib[n]
 
+
 # Fibonacci function using recursion
 def fibonacci_recursive(n):
     if n <= 0:
@@ -18,3 +22,21 @@ def fibonacci_recursive(n):
         return 1
     else:
         return fibonacci_recursive(n - 1) + fibonacci_recursive(n - 2)
+
+
+@click.command()
+@click.option('--mode', type=click.Choice(['rec', 'ite']), default='ite')
+@click.option('--n', default=10)
+def main(mode: str, n: int):
+    fibonacci_functions = {
+        'rec': fibonacci_recursive,
+        'ite': fibonacci_iterative
+    }
+
+    result = fibonacci_functions.get(mode)(n)
+
+    print(result)
+
+
+if __name__ == "__main__":
+    main()
