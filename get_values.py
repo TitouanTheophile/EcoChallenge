22caba78d2f450efbf76a9255582eccd76ce8631
m-bonati <69583646+m-bonati@users.noreply.github.com>
2024-05-30 11:59:23 +0200

    main script

---
+++ get_values.py
@@ -0,0 +1,42 @@
+config = {
+    "sum": {
+
+    },
+    "hex": {
+
+    },
+    "abs": {
+        "nb_exec": 2,
+        "scenarios": [
+            {"x": 1},
+            {"x": 10},
+            {"x": -10},
+            {"x": 20}
+        ]
+    },
+    "len": {
+
+    },
+    "divmod": {
+
+    },
+    "enumerate": {
+
+    },
+}
+
+builtin_list = ['sum', 'hex', 'abs', 'len', 'divmod', 'enumerate']
+
+
+def main():
+    for builtin in builtin_list:
+        results = {
+            'joule_ram': [],
+            'joule_cpu': [],
+        }
+
+        builtin_scenarios = config[builtin]
+
+        builtin_custom_path = f'custom/{builtin}.py'
+
+
