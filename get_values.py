2594ef4aa9515b76f2ecc2a386c51c99644aaf33
Seb Prat <seb_prat@yahoo.fr>
2024-05-30 14:41:31 +0200

    Add new tests

--- get_values.py
+++ get_values.py
@@ -4,16 +4,16 @@ import pandas as pd
 
 builtin_list = [
     'sum'
-#    'hex',
-#    'abs',
-#    'len',
-#    'divmod',
-#    'enumerate',
-#    'map',
+    'hex',
+    'abs',
+    'len',
+#   'divmod',
+    'enumerate',
+    'map',
 #    'map_build_list',
 #    'map_genexp',
 #    'map_list_comp',
-#    'filter',
+     'filter',
 #    'filter_build_list',
 #    'filter_genexp',
 #    'filter_list_comp',
