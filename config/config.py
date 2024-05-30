bd7cf849412b11b7575c920475da56eb2d4b7ace
Tisola <10808786+TitouanTheophile@users.noreply.github.com>
2024-05-30 12:00:37 +0200

    Use a single file for configuration

---
+++ config/config.py
@@ -0,0 +1,48 @@
+conf = {
+    "abs": {
+        "scenarios": [
+            {
+                "x": 1
+            },
+            {
+                "x": 10
+            },
+            {
+                "x": -10
+            },
+            {
+                "x": 20
+            }
+        ]
+    },
+    "sum": {
+        "scenarios": [
+            {
+            }
+        ]
+    },
+    "hex": {
+        "scenarios": [
+            {
+            }
+        ]
+    },
+    "map": {
+        "scenarios": [
+            {
+            }
+        ]
+    },
+    "filter": {
+        "scenarios": [
+            {
+            }
+        ]
+    },
+    "zip": {
+        "scenarios": [
+            {
+            }
+        ]
+    },
+}
