472fa9951ab0b47325af2eac601f756a77687790
Tisola <10808786+TitouanTheophile@users.noreply.github.com>
2024-05-30 14:12:02 +0200

    update conf

--- config/config.py
+++ config/config.py
@@ -1,90 +1,72 @@
 conf = {
-    "abs": {
-        "scenario_args": [
-            (1,),
-            (-1,),
-            (10,),
-            (-10,),
-            (-9_223_372_036_854_775_808,),
-        ]
-    },
-    "hex": {
-        "scenario_args": [
-            (1,),
-            (-1,),
-            (10,),
-            (-10,),
-            (-9_223_372_036_854_775_808,),
-        ]
-    },
-    "sum": {
+    "abs": [
+        (1,),
+        (-1,),
+        (10,),
+        (-10,),
+        (-9_223_372_036_854_775_808,),
+    ],
+    "hex": [
+        (1,),
+        (-1,),
+        (10,),
+        (-10,),
+        (-9_223_372_036_854_775_808,),
+    ],
+    "sum": [
         # Takes size of the list that will be generated with range
-        "scenario_args": [
-            (1,),
-            (20,),
-            (100,),
-            (500,),
-            (5000,),
-        ]
-    },
-    "map": {
+        (1,),
+        (20,),
+        (100,),
+        (500,),
+        (5000,),
+    ],
+    "map": [
         # Takes size of the list that will be generated with range
-        "scenario_args": [
-            (1,),
-            (20,),
-            (100,),
-            (500,),
-            (5000,),
-        ]
-    },
-    "filter": {
+        (1,),
+        (20,),
+        (100,),
+        (500,),
+        (5000,),
+    ],
+    "filter": [
         # Takes size of the list that will be generated with range
-        "scenario_args": [
-            (1,),
-            (20,),
-            (100,),
-            (500,),
-            (5000,),
-        ]
-    },
-    "zip": {
+        (1,),
+        (20,),
+        (100,),
+        (500,),
+        (5000,),
+    ],
+    "zip": [
         # Takes size of the list that will be generated with range
-        "scenario_args": [
-            (1, 1),
-            (20, 20),
-            (100, 100),
-            (500, 300),
-            (300, 150),
-        ]
-    },
-    "enumerate": {
+        (1, 1),
+        (20, 20),
+        (100, 100),
+        (500, 300),
+        (300, 150),
+    ],
+    "enumerate": [
         # Takes size of the list that will be generated with range
-        "scenario_args": [
-            (1,),
-            (20,),
-            (100,),
-            (500,),
-            (300,),
-        ]
-    },
-    "any": {
+        (1,),
+        (20,),
+        (100,),
+        (500,),
+        (300,),
+    ],
+    "any": [
         # Takes size of the list that will be generated with range
-        "scenario_args": [
-            (1,),
-            (20,),
-            (100,),
-            (500,),
-            (300,),
-        ]
-    },
-    "all": {
+        (1,),
+        (20,),
+        (100,),
+        (500,),
+        (300,),
+    ],
+    "all": [
         # Takes size of the list that will be generated with range
-        "scenario_args": [
-            (1,),
-            (20,),
-            (100,),
-            (500,),
-            (300,),
-        ]
-    },
+        (1,),
+        (20,),
+        (100,),
+        (500,),
+        (300,),
+    ],
 }
